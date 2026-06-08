55fcd569e65bc9ca4d01a442c2ab27df2437b432
ThaRSan1101 <arultharisan01@gmail.com>
2026-06-08 16:48:50 +0530

    resolve procument dashbord

--- frontend/src/pages/inventory/ProcurementManagement/ProcurementManagement.tsx
+++ frontend/src/pages/inventory/ProcurementManagement/ProcurementManagement.tsx
@@ -286,7 +286,7 @@ export default function ProcurementManagement() {
 
 
   useEffect(() => {
-    window.localStorage.setItem(SUPPLIER_STORAGE_KEY, JSON.stringify(suppliersList));
+    window.localStorage.setItem(SUPPLIERS_STORAGE_KEY, JSON.stringify(suppliersList));
   }, [suppliersList]);
 
   useEffect(() => {
