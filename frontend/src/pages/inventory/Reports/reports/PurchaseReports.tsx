eb044e548e8b7d349983470813ce9e40dfe60da4
ThaRSan1101 <arultharisan01@gmail.com>
2026-06-14 16:59:47 +0530

    .

--- frontend/src/pages/inventory/Reports/reports/PurchaseReports.tsx
+++ frontend/src/pages/inventory/Reports/reports/PurchaseReports.tsx
@@ -1,4 +1,4 @@
-.import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { downloadReport, ViewState } from './reportUtils';
 import { inventoryOperationsService } from '../../StockOperations/operations/inventoryOperationsService';
 
